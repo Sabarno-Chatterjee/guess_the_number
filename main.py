c33c2cd39ab64047e0c8711f4236db3b6c055c7c
Sabarno Chatterjee <106575210+Sabarno-Chatterjee@users.noreply.github.com>
2022-09-27 21:25:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
 import random
 import art
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS =5
+
+#Function to check user's guess
+def check_answer(guess, pick, lives):
+    """Checks users guess, returns the number of remaining lives"""
+    if guess > pick:
+        print("That's a bit high! Take your shot again.")
+        return lives - 1
+    elif guess < pick:
+        print("That's a bit low! Take your shot again.")
+        return lives - 1
+    else:
+        print(f"You won! The answer was {pick}")
+
+#Function to set difficulty
+def set_difficulty():
+    choice = input("Select a level of difficulty, easy or hard.\n").lower()
+    if choice == "easy":
+        return EASY_LEVEL_TURNS
+    elif choice == "hard":
+        return HARD_LEVEL_TURNS
+
 print(art.logo)
 
-numbers = []
-for number in range(1, 101):
-    numbers.append(number)
+def game():
+    print("Welcome to the number guessing game.")
+    print("I'm thinking of a number between 1 and 100.")
+    pick = random.randint(1, 100)
+    #Test code:
+    #print(f"The correct answer is {pick}")
+    lives = set_difficulty()
 
-pick = random.choice(numbers)
+    guess = 0
 
-print(pick)
-
-is_match = False
-choice = input("Select a level of dificulty, easy or hard.\n").lower()
-if choice == "easy":
-    lives = 10
-elif choice == "hard":
-    lives = 5
-else:
-    print("Invalid entry.\n")
-print()
-print(f"Current number of lives: {lives}.")
-while not is_match:
-    guess = int(input("Guess a number between 1 to 100.\n"))
-    if guess > pick:
-        lives -= 1
+    while guess != pick:
+        print(f"Current number of lives are {lives}.")
+        #Lets the user take a guess
+        guess = int(input("Guess a number.\n"))
+        lives = check_answer(guess, pick, lives)
         if lives == 0:
-            print(f"You lost, the correct number was {pick}.")
-            is_match = True
-        else:
-            print("That's a bit high! Take your shot again.")
-            print(f"Remaining number of lives are {lives}.")
-            print()
-    elif guess < pick:
-        lives -= 1
-        if lives == 0:
-            print(f"You lost, the correct number was {pick}.")
-            is_match = True
-        else:
-            print("That's a bit low! Take your shot again.")
-            print(f"Remaining number of lives are {lives}.")
-            print()
-    elif guess == pick:
-        print(f"You won! The answer was {pick}")
-        is_match = True
+            print("Your are out of turns, you lose")
+            return
+        elif guess != pick:
+            print("Guess again")
+
+
+game()
+
+
+
+
